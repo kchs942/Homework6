8819938ca5676c6729b4e54ca88f70e4941ef528
kchs942 <kchs942@naver.com>
2016-10-28 16:13:08 +0900

    ExamVariousArray 파일 생성및 구현

---
+++ homework6/src/homework6/ExamVariousArray.java
@@ -0,0 +1,43 @@
+package homework6;
+
+import java.util.*;
+public class ExamVariousArray {
+
+	public static void main(String[] args) {
+		int number;
+		Scanner s=new Scanner(System.in);
+	    do{
+	    	System.out.println("1. 두번째로 큰 수 찾기");
+	    	System.out.println("2. 심사 점수 계산");
+	    	System.out.println("3. 학생 총점,평균값 구하기");
+	    	System.out.println("4. 5층 아파트의 거주자 숫자 구하기");
+	    	System.out.println("5. 겹치지 않는 숫자 10개 입력받기");
+	    	System.out.println("6. 겹치지 않는 숫자 10개 입력받기");
+	    	System.out.println("원하는 메뉴는>>");
+	    	number=s.nextInt();
+	    	
+	    		if(number==1)
+	    			new SecondMax().input();
+	    		
+	    		else if(number==2)
+	    			new AvgScore().input();
+	    		
+	    		else if(number==3)
+	    			new StudentScore().input();
+	    		
+	    		else if(number==4)
+	    			new AptPersonCount().input();
+	    		
+	    		else if(number==5)
+	    			new OnlyNumber().input();
+	    		
+	    		
+	   	    	
+	    }
+	    while(number!=6); 
+	    System.out.print("종료하셨습니다.");
+	   
+
+	}
+
+}
